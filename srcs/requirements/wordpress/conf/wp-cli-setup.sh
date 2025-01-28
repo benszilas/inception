c9d72769ed4fce1e12f15ab70992b39baad3bb7c
bszilas <szbengio@gmail.com>
2025-01-28 12:04:15 -0500

    to build nginx image from Dockerfile, create certificate

--- srcs/requirements/wordpress/conf/wp-cli-setup.sh
+++ srcs/requirements/wordpress/conf/wp-cli-setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-wp core download
-wp config create --dbname=wp_db --dbuser=wp_user --dbpass=wp_password --dbhost=db
+wp core download --allow-root
+wp config create \
+	--dbname="$WORDPRESS_DB_NAME" \
+	--dbuser="$WORDPRESS_DB_USER" \
+	--dbpass="$WORDPRESS_DB_PASSWORD" \
+	--dbhost="$WORDPRESS_DB_HOST" \
+	--allow-root
 wp core install \
-	--url="http://localhost:8080" \
+	--url=http://localhost \
 	--title="Hello World" \
-	--admin_user="admin" \
-	--admin_password="strongpassword" \
-	--admin_email="admin@example.com"
+	--admin_user="$WP_ADMIN" \
+	--admin_password="$WP_ADMIN_PW" \
+	--admin_email="$WP_ADMIN_EMAIL" \
+	--allow-root
+wp user create "$WP_SECOND_USER" "$WP_SECOND_USER_PW" --allow-root
+
+php-fpm
