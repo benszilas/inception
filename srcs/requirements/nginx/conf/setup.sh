842535fe60b7de10b24784e554438a0862455db3
Beni <szbengio@gmail.com>
2025-02-17 11:30:36 -0500

    nginx not working with wordpress files

--- srcs/requirements/nginx/conf/setup.sh
+++ srcs/requirements/nginx/conf/setup.sh
@@ -1,26 +1,15 @@
 #!/bin/sh
 
-cat <<-EOF > /var/www/html/index.html
-    <html>
-        <head>
-            <title>Welcome to $DOMAIN_NAME</title>
-        </head>
-        <body>
-            <p>This is the fallback web page for this server.</p>
-            <p>The web server is running but wordpress files are not reachable.</p>
-        </body>
-    </html>
-EOF
+envsubst '$SSL_CERT_FILE $SSL_KEY_FILE $DOMAIN_NAME' < /tmp/"$DOMAIN_NAME".conf > /etc/nginx/http.d/"$DOMAIN_NAME".conf
+echo server config copied with status: $?
 
-if [ ! -f /etc/nginx/nginx.conf.orig ]; then
-    mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
-fi
-
-envsubst '$SSL_CERT_FILE $SSL_KEY_FILE $DOMAIN_NAME' < /tmp/nginx.conf > /etc/nginx/nginx.conf
-
+#self-signed certificate for testing purposes - therefore ok to use this backup
 if [ ! -f "$SSL_CERT_FILE" ] || [ ! -f "$SSL_KEY_FILE" ]; then
+    echo "Generating self-signed certificate..."
 	rm -rf "$SSL_CERT_FILE" "$SSL_KEY_FILE"
     openssl req -new -x509 -nodes -out "$SSL_CERT_FILE" -keyout "$SSL_KEY_FILE"
+else
+    echo "Using provided certificate..."
 fi
 
 "$@"
